c5a6852b32b46e8b153376f8c64e77fce580ce4e
devwiandj@gmail.com <devWianDJ@gmail.com>
2023-02-21 12:05:27 +0200

    added webpack config

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { render } from 'react-dom'
+window.React = React
 import { Provider } from "react-redux";
 import { store, persistor } from "./store/index";
-import App from "./App";
+import App from "./App.jsx";
 import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
